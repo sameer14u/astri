c64f64ca6aa861e12bab2dcd8e3db731cbc1a5e3
sameer choudhary <sameerchoudhary@fizzz.local>
2025-05-01 12:07:09 +0530

    nextcode

---
+++ est lum.py
@@ -0,0 +1,4 @@
+magnitude = {17.3, 15.8, 16.5}
+redshift = {0.12,0.08,0.15}
+for mag, z in zip(magnitude, redshift):
+    print(f"Magnitude : {mag}, Redshift : {z}")
---
+++ mag to flux.py
@@ -0,0 +1,6 @@
+galaxies = {17.3,15.8,16.5}
+for mag in galaxies:
+    flux = 10**(-0.4*mag)
+    print (flux)
+    #converting mag into flux
+    
